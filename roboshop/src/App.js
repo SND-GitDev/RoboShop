5e9d6af4df8ab473b7bf1d3241fdb513fa92b2de
SND_Dev <sws.indiedev@gmail.com>
2022-05-09 04:14:55 +0300

    COMPLETED Task 1

---
+++ roboshop/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import logo from './logo192.png'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>RoboShop</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          All your robot needs in one place
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
